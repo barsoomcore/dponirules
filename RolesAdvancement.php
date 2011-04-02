f22fdecb6195830e631cd64ec56b1b418167cea5
Corey <coreyswan@Hispaniola.local>
2011-04-02 15:29:54 -0400

    Added Improved Resilience, adjusted powers list.

--- RolesAdvancement.php
+++ RolesAdvancement.php
@@ -8,19 +8,20 @@
 <h1>LEVEL ADVANCEMENT</h1>
 <p>The Narrator will tell you when your hero advances in level. When this happens, do the following:</p>
 <ul>
-	<li>Decide whether to add a level to your hero's existing role (or one of your hero's existing roles, for mixed-role heroes) or to add a level in a new role. See the next section, Mixed-Role Heroes, for details on this.</li>
-	<li>Look at the <a href="Roles.php">Level-Dependent Benefits table</a> and the level table for your hero's role(s). Note any increases to <a href="Combat.php">combat bonus</a>, <a href="BasicsSavingThrows.php">saving throws</a> and <a href="Skills.php">skill ranks</a>.</li>
+	<li>Decide which role to increase by a level. If you choose a new role, refer to the "Mixed-Role Heroes" section, below.</li>
+	<li>Note any increases to <a href="Combat.php">combat bonus</a>, <a href="BasicsSavingThrows.php">saving throws</a>, <a href="Skills.php">skill ranks</a> and <a href="PowersUsing.php">power ranks</a>.</li>
 	<li>Pick a new <a href="Feats.php">feat</a> for your hero from among the <a href="Feats.php">feats</a> available for the new role level.</li>
 	<li>If your hero's new total level is divisible by six (6th, 12th, or 18th), increase one of your hero's ability scores by 1.</li>
 </ul>
 
 <h1>Mixed-Role Heroes</h1>
-<p>Heroes may acquire other roles as they progress in level, becoming mixed role heroes. The traits from a hero's different roles combine, so a mixed role hero has versatility at the expense of focus.</p>
+<p>Heroes may acquire other roles as they progress in level, becoming mixed-role heroes. The traits from a hero's different roles combine, so a mixed role hero has versatility at the expense of focus.</p>
 <p>As a general rule, the traits of a mixed-role hero are the sum of the traits of each of the hero's roles, as follows:</p>
 <p><strong>Level:</strong> <em>Total level</em> is a character's total number of levels in all roles. For example, a hero who is a 2nd-level warrior and 1st-level adept has a total level of 3rd. Total level is used to determine a hero's benefits on the Level-Dependent Benefits table.</p>
 <p><em>Role level</em> is a hero's level in a particular role. For a hero whose levels are all in the same role, total level and role level are the same thing. Role level is used to determine the hero's benefits from each particular role.</p>
 <p><strong><a href="Combat.php">Combat Bonus</a>:</strong> Add the combat bonuses for each role together to get the hero's total combat bonus.</p>
-<p><strong><a href="BasicsSavingThrows.php">Saving Throws</a>:</strong> Add the save bonuses for each role together. Remember the first-level bonus you received for your initial character level. Note that if you take levels in both Expert and Generalist, they must share the same “Good” save.</p>
+
+<p><strong><a href="BasicsSavingThrows.php">Saving Throws</a>:</strong> Add the save bonuses for each role together. Remember the first-level bonus you received for your initial character level. Note that if you take levels in both Expert and Generalist, they must share the same "Good" save.</p>
 
 <p><strong><a href="Powers.php">Power Ranks</a>:</strong> If a hero takes ranks in both the adept and generalist roles, they determine their total power rank by adding the ranks from each role together, and then subtracting 2. So a character with 5 levels of adept (8 ranks) and 5 levels of generalist (5 ranks), has total power ranks of 11 ((8+5)-2).</p>
 <p><strong><a href="Skills.php">Skills</a>:</strong> If you add a level in a new role for your hero, you may gain the number of ranks that role allows per level.</p>
