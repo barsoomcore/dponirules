192fd191fe527233db6ba99ae0cbef3f3c3593b6
Corey <coreyswan@Hispaniola.local>
2011-11-20 18:12:36 -0500

    Further trimming

--- BasicsStunts.php
+++ BasicsStunts.php
@@ -9,26 +9,16 @@
 
 <p>Imaginative players can come up with ways for their characters to use their skills in varied situations. The Stunt rule provides a basic guideline for adjudicating such uses, and to encourage players to be imaginative and creative in their combat choices.</p>
 
-<p>When performing a stunt, characters can receive a bonus to one of their checks in a round by spending a move action using one of their skills*. They do not make a check with that skill; the bonus received is equal to the total bonus of the skill they are stunting with (including ranks, ability modifiers and any miscellaneous bonuses) divided by 5, rounded up. A character must have at least one rank in a skill to use it for stunting.</p>
+<p>When performing a stunt, characters can receive a bonus to one of their checks in a round by using one of their skills. They do not make a check with that skill; the bonus received is equal to the total bonus of the skill they are stunting with (including ranks, ability modifiers and any miscellaneous bonuses) divided by 5, rounded up. A character must have at least one rank in a skill to use it for stunting.</p>
 
 <p class="notation">Stunt Bonus = (Skill Bonus / 5) Round UP</p>
 
-<p>The player must describe (in a more or less convincing manner) how the skill is being used in order to receive the stunt bonus. A stunt bonus may be conceived of and applied AFTER the results of the check are known. The GM is the final arbiter of stunts and may disallow any suggested stunt at her discretion. The stunt bonus may be applied to any ONE of the following:</p>
-<ul>
-	<li>A <a href="Skills.php">Skill Check</a> or <a href="BasicsAbilities.php">Ability Check</a></li>
-	<li>An <a href="Combat.php#AttackBonus">Attack Roll</a> or <a href="Combat.php#ManeuverBonus">Combat Maneuver Check</a></li>
-	<li>A <a href="BasicsSavingThrows.php">Fortitude, Reflex, or Will Saving Throw</a> (but NOT Toughness)</li>
-	<li><a href="Combat.php#BaseDefense">Defense</a></li>
-	<li><a href="Powers.php">Power Ranks</a></li>
-</ul>
-<p>Note that you may not apply stunt bonuses to Toughness saves or any sort of Recovery check (whether for <a href="CombatDamage.php">Damage</a> or <a href="BasicsConviction.php">Conviction</a>).</p>
+<p>The player must describe (in a more or less convincing manner) how the skill is being used in order to receive the stunt bonus. A stunt bonus may be conceived of and applied AFTER the results of the check are known. The GM is the final arbiter of stunts and may disallow any suggested stunt at her discretion. The stunt bonus may be applied to <a href="Skills.php">Skill Checks</a>, <a href="BasicsAbilities.php">Ability Checks</a>, <a href="Combat.php#AttackBonus">Attack Rolls</a>, or <a href="Combat.php#ManeuverBonus">Combat Maneuver Rolls</a>. </p>
 
-<h2>Notes</h2>
-<p><b>Restriction:</b> Each time a skill is used for a stunt bonus in an encounter, that skill's stunt bonus is reduced by one the next time it is used.</p>
+<p>Note that you may not apply stunt bonuses to <a href="BasicsSavingThrows.php">Saving Throws</a> or any sort of Recovery check (whether for <a href="CombatDamage.php">Damage</a> or <a href="BasicsConviction.php">Conviction</a>).</p>
 
-<p><b>Special:</b> You can use <a href="BasicsChecks.php#Challenges">the Fast Task Challenge</a> to reduce the move action to a Free Action. Applying the normal -5 challenge penalty on the skill to the Stunt Bonus calculation means the stunt bonus is one less.</p>
-
-<p>Stunts using movement-based skills (<a href="Skills.php?Name=Acrobatics">Acrobatics</a> in particular), occur as part of that movement.</p>
+<h2>Restriction</h2>
+<p>Each time a skill is used for a stunt bonus in <a href="BasicsTime.php">a scene</a>, that skill's stunt bonus is reduced by one the next time it is used.</p>
 
 </div>
 </body>
